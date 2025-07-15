41911a2d27f250a8b55a5854024b622f00fa6ff4
NguyenQuocHao <nguyenquochao2000@gmail.com>
2025-07-15 03:04:13 -0400

    refactor: extract Router to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css'
 import Sidebar from './components/Sidebar'
-import { BrowserRouter, Routes, Route } from "react-router";
-import { EditFlashCard } from './views/EditFlashCard'
-import { HOME_PATH, MY_LISTS_PATH, VIEW_FLASHCARD_PATH, EDIT_FLASHCARD_PATH, ADD_FLASHCARD_PATH } from './const/paths.tsx'
+import Router from './components/Router'
 
 function App() {
 
@@ -10,15 +8,7 @@ function App() {
     <>
       <Sidebar />
 
-      <BrowserRouter>
-        <Routes>
-          <Route path={HOME_PATH} element={<></>} />
-          <Route path={MY_LISTS_PATH} element={<></>} />
-          <Route path={VIEW_FLASHCARD_PATH} element={<EditFlashCard />} />
-          <Route path={ADD_FLASHCARD_PATH} element={<EditFlashCard />} />
-          <Route path={`${EDIT_FLASHCARD_PATH}/:word`} element={<EditFlashCard />} />
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </>
   )
 }
---
+++ src/components/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { EditFlashCard } from '../views/EditFlashCard'
+import { HOME_PATH, MY_LISTS_PATH, VIEW_FLASHCARD_PATH, EDIT_FLASHCARD_PATH, ADD_FLASHCARD_PATH } from '../const/paths.tsx'
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={HOME_PATH} element={<></>} />
+                <Route path={MY_LISTS_PATH} element={<></>} />
+                <Route path={VIEW_FLASHCARD_PATH} element={<EditFlashCard />} />
+                <Route path={ADD_FLASHCARD_PATH} element={<EditFlashCard />} />
+                <Route path={`${EDIT_FLASHCARD_PATH}/:word`} element={<EditFlashCard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
